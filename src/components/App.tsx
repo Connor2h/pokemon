bdd5ecc2a6c214dce11b0dad618203ebfd37f4f0
Connor Hollis <connorhollis3d@gmail.com>
2026-02-23 20:28:27 -0600

    setting up initial click to get pokemon with mock data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 
+
 export function App() {
-  const [count, setCount] = useState(0)
+
+  interface Pokemon {
+    name: string;
+    url: string;
+  }
+
+  const pokemons: Pokemon[] = [
+    { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+    { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
+    { name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" }
+  ];
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <button onClick={() => console.log(pokemons)}>Click Me</button>
     </>
   )
 }
