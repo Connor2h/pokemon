68d6d65c49c16034060d049a4202b2eb2b122fee
Connor Hollis <connorhollis3d@gmail.com>
2026-02-24 20:09:29 -0600

    added another pokemon type for images and cries

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,19 @@ export interface Pokemon {
 export interface PokemonListResponse{
   results: Pokemon[];
 }
+
+export interface PokemonDetail{
+  name: string;
+  cries?: {
+    latest?: string | null;
+    legacy?: string | null;
+  };
+  sprites: {
+    front_default: string | null;
+    other?: {
+      ['official-artwork']?: {
+        front_default: string | null;
+      };
+    };
+  };
+};
