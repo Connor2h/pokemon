b0f713fe562819bd91d72daadea7693d98c9d409
Connor Hollis <connorhollis3d@gmail.com>
2026-02-24 18:21:12 -0600

    created hooks directory

--- src/hooks/usePokemonData.tsx
+++ src/hooks/usePokemonData.tsx
@@ -11,4 +11,25 @@ export function usePokemonListQuery(): UseQueryResult<Pokemon[], Error>{
       return pokemonObjects.results;
     },
   });
-}
+};
+
+// export type UsePokemonListQueryOptions<TData = Pokemon[]> = Omit<
+//   UseQueryOptions<Pokemon[], Error, TData>,
+//   'queryKey' | 'queryFn'
+// >;
+
+// Wrapper for useQuery that allows you to pass in a optional options object
+// https://tkdodo.eu/blog/react-query-data-transformations
+// Can be used like usePokemonListQuery({select: (data) => data.map((todo) => todo.name.toUpperCase())});
+// export const usePokemonListQuery = <TData = Array<Pokemon>>(options?: UsePokemonListQueryOptions<TData>): UseQueryResult<TData, Error> => {
+//   return useQuery({
+//     queryKey: ['pokemons'],
+//     queryFn: async (): Promise<Array<Pokemon>> => {
+//       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+//       const pokemonObjects: PokemonListResponse = await response.json();
+//       console.log(pokemonObjects.results);
+//       return pokemonObjects.results;
+//     },
+//     ...options,
+//   });
+// };
